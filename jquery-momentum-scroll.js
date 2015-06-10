84da4d668d47a38210200093a0bcef24412b6828
Bryan Lusica <dev@yourwebtech.info>
2015-06-10 12:41:39 +0800

    Update jquery-momentum-scroll.js

--- jquery-momentum-scroll.js
+++ jquery-momentum-scroll.js
@@ -1,7 +1,7 @@
 /**
  * jQuery Momentum Scroll
  * This will transform the native scroll of the browser into a very smooth scroll with momentum effect
- * https://github.com/iahnn/jQuery-Kinetic-Scroll/
+ * https://github.com/iahnn/jQuery-Momentum-Scroll
  * licensed under MIT
  * version 1.0
  */
@@ -10,44 +10,52 @@ jQuery(function($) {
     "use strict";
 
     var win = $(window)
-        , target = $('body')
-        , wrapper = target.find('> div')
-        , easing = "ease-out" //css easing
-        , duration = "1.2s" //duration ms(millisecond) or s(second)
-        , top = 0
-        , kineticScroll = {
-            _init: function() {
-                if( wrapper.length == 1 ) {
-                    target.height(wrapper.height());
-                    wrapper.css({
-                        transition: 'transform ' + duration + ' ' + easing,
-                        position: 'fixed',
-                        top: '0',
-                        left: '0',
-                        width: '100%',
-                        padding: '0',
-                        zIndex: '2'
-                    });
+            , target = $('body')
+            , wrapper = target.find('> div')
+            , easing = "ease-out" //css easing
+            , duration = "1.2s" //duration ms(millisecond) or s(second)
+            , top = 0
+            , jmScroll = {
+                _init: function() {
+                    if( wrapper.length == 1 ) {
 
-                    win.on({
-                        scroll: function () {
-                            kineticScroll._scroll();
-                        }
-                        , resize: function() {
-                            target.height(wrapper.outerHeight());
-                        }
-                    });
+                        jmScroll._resize();
 
-                    kineticScroll._scroll();
+                        wrapper.css({
+                            transition: 'transform ' + duration + ' ' + easing,
+                            position: 'fixed',
+                            top: '0',
+                            left: '0',
+                            width: '100%',
+                            padding: '0',
+                            zIndex: '2',
+                            display: 'block',
+                            backfaceVisibility: 'hidden'
+                        });
+
+                        jmScroll._scroll();
+                    }
+                },
+                _scroll: function() {
+                    top = win.scrollTop();
+                    wrapper.css('transform', 'translateY(-' + top + 'px)');
+                },
+                _resize: function() {
+                    target.height(wrapper.outerHeight());
                 }
-            },
-            _scroll: function() {
-                top = win.scrollTop();
-                wrapper.css('transform', 'translateY(-' + top + 'px)');
-            }
-        };
+            };
 
     if (typeof window.ontouchstart == 'undefined') {
-        kineticScroll._init();
+        win.on({
+            scroll: function () {
+                jmScroll._scroll();
+            }
+            , resize: function() {
+                jmScroll._resize();
+            }
+            , load: function() {
+                jmScroll._init();
+            }
+        });
     }
 });
